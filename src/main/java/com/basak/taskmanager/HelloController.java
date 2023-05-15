c300e3be00b873a24fa8e4bd966f4d50b8379099
Sourav <sbasak2@apple.com>
2023-05-15 14:25:45 +0530

    Task manager first commit

---
+++ src/main/java/com/basak/taskmanager/HelloController.java
@@ -0,0 +1,18 @@
+package com.basak.taskmanager;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    String hello() {
+        return "Hello World!";
+    }
+
+
+    pub
+
+}
